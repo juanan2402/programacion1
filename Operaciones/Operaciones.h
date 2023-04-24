7c775dc4175670bb95cfbab90eb6c3eb1b8ccfd4
juanan2402 <juananserrano2402@udla.edu.ec>
2023-04-24 14:41:36 -0500

    changes json

--- Operaciones/Operaciones.h
+++ Operaciones/Operaciones.h
@@ -2,5 +2,3 @@ int Sumar(int s1, int s2);
 int Restar(int s1, int s2);
 int Multiplicar(int s1, int s2);
 float Dividir(int s1, int s2);
-int Menu();
-void Imprimir();
--- Operaciones/main.c
+++ Operaciones/main.c
@@ -1,6 +1,39 @@
 #include<stdio.h>
 #include "Operaciones.h"
+int Menu(){
+    int operacion, d1, d2, resultado;
+    printf("Escoga una operacion:\n 1- Suma \n 2- Resta\n 3- Multiplicacion \n 4- Division \n 5- Salir \n");
+        scanf("%d",&operacion);
+        if(operacion!=5){
+        printf("Ingresar los datos: \n ");
+        scanf("%d %d", &d1, &d2);
+        switch (operacion)
+        {
+        case 1:
+            resultado = Sumar(d1, d2);
+            
+            break;
+        case 2:
+            resultado = Restar(d1, d2);
+            
+            break;
+        case 3:
+            resultado = Multiplicar(d1, d2);
+            
+            break;
+        case 4:
+            resultado = Dividir(d1, d2);
+            
+            break;
+        }
+        Imprimir(resultado);
+        }
+        return operacion;
+}
+void Imprimir(float dato){
+    printf("El resultado de la operacion es: %0.2f \n", dato);
 
+}
 int main(){
     int input;
     

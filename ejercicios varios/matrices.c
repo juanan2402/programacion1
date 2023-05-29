0a352bfc24f3ae464707af30c622e703f189215e
juanan2402 <juananserrano2402@gmail.com>
2023-05-29 14:45:15 -0500

    comit matrices

---
+++ ejercicios varios/matrices.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#define n 3
+
+
+
+void LeerMatriz(int fil, int col, int matriz[fil][col]){
+    for(int i=0; i<fil; i++){
+        for(int j=0; j<col; j++){
+            printf("Ingresar el dato en la posicion %d,%d de la matriz \n", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+}
+void ImprimirMatriz(int fil, int col, int matriz[fil][col]){
+    for(int i=0; i<fil; i++){
+        for(int j=0; j<col; j++){
+            printf(" %d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+void SumaMatriz(int fil, int col, int matrizA[fil][col], int matrizB[fil][col], int matrizC[fil][col]){
+    for(int i=0; i<fil; i++){
+        for(int j=0; j<col; j++){
+            matrizC[i][j]=matrizA[i][j]+matrizB[i][j];
+        }
+    }
+
+}
+
+int main(){
+    
+    int matA[10][10];
+    
+    int matB[10][10];
+
+    int matC[10][10];
+    printf("Matriz A\n");
+    LeerMatriz(n, n, matA);
+    printf("Matriz B\n");
+    LeerMatriz(n,n,matB);
+    SumaMatriz(n,n,matA,matB,matC);
+    ImprimirMatriz(n,n,matC);
+
+    
+    
+
+}

ed2b7dad44829b0ec2cd4faaa795c0007c8ed3a2
juanan2402 <86845266+juanan2402@users.noreply.github.com>
2023-06-21 14:16:29 -0500

    Update pstrings.c

--- punteros/pstrings.c
+++ punteros/pstrings.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int comparacion(char* pal1, char* pal2){
+  while(*pal1 && *pal2){
+    if(*pal1 != *pal2){
+      return 0;
+    }
+    pal1++;
+    pal2++;
+    else{
+      if(*pal1==0 && *pal2 == 0){
+        return 1; 
+      }
+    }
+  }
+}
